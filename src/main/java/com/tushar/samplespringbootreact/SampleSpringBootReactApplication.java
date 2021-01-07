966f2bad76db1724ff17268a7bf2204e94e1e68c
tsahay2 <tsahay2@gmail.com>
2021-01-07 21:22:31 +0530

    Init commit

---
+++ src/main/java/com/tushar/samplespringbootreact/SampleSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.tushar.samplespringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tushar/samplespringbootreact/SampleSpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.samplespringbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
