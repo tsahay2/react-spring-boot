41eb19f8e094e49c0ffaea5aa9ea422faa64a2f2
tsahay2 <tsahay2@gmail.com>
2021-01-08 02:36:14 +0530

    @tsahay Adding semantic UI + Saas support.

---
+++ ui/src/containers/home-page/HomePage.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import { Header } from 'semantic-ui-react'
+
+export class HomePage extends Component{
+
+    render(){
+        return(
+            <div className="page-wrapper">
+                <Header as='h1'>Sex with me is like</Header>
+
+            </div>
+        )
+    }
+
+}
